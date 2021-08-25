57adad0dd924297c638d763d5b04c4849e45527a
SOunit <workbookwork7@gmail.com>
2021-08-25 13:47:20 -0700

    add query to graphql fetching data

--- 02_query/graphql_basics/src/index.js
+++ 02_query/graphql_basics/src/index.js
@@ -1,12 +1,55 @@
 import { GraphQLServer } from 'graphql-yoga';
 
+// Dummy data
+const users = [
+  {
+    id: '1',
+    name: 'Jack Pearson',
+    email: 'jack@example.com',
+    age: 38,
+  },
+  {
+    id: '2',
+    name: 'rebecca Pearson',
+    email: 'rebecca@example.com',
+    age: 38,
+  },
+  {
+    id: '3',
+    name: 'kevin Pearson',
+    email: 'kevin@example.com',
+    age: 14,
+  },
+];
+
+const posts = [
+  {
+    id: '1',
+    title: 'title 1',
+    body: 'body 1',
+    published: true,
+  },
+  {
+    id: '2',
+    title: 'title 2',
+    body: 'body 2',
+    published: true,
+  },
+  {
+    id: '3',
+    title: 'title 3',
+    body: 'body 3',
+    published: true,
+  },
+];
+
 // Type Definition (Schema)
 const typeDefs = `
     type Query {
-      greeting(name: String):String!
+      users(query: String): [User!]!
+      posts(query: String): [Post!]!
       me: User!
       post: Post!
-      add(a: Float!, b: Float!): Float!
     }
 
     type User {
@@ -27,15 +70,33 @@ const typeDefs = `
 // Resolvers
 const resolvers = {
   Query: {
-    add(parent, args) {
-      return args.a + args.b;
-    },
-    greeting(parent, args, ctx, info) {
-      if (args.name) {
-        return `Hello, ${args.name}`;
-      } else {
-        return 'Hello';
+    users(parent, args, ctx, info) {
+      if (!args.query) {
+        return users;
       }
+      // true for stay
+      // false for drop
+      return users.filter((user) => {
+        return user.name.toLowerCase().includes(args.query.toLowerCase());
+      });
+    },
+    posts(parent, args, ctx, info) {
+      const query = args.query;
+
+      if (!query) {
+        return posts;
+      }
+
+      return posts.filter((post) => {
+        const isTitleMatch = post.title
+          .toLowerCase()
+          .includes(query.toLowerCase());
+        const isBodyMatch = post.body
+          .toLowerCase()
+          .includes(query.toLowerCase());
+
+        return isTitleMatch || isBodyMatch;
+      });
     },
     me() {
       return {
