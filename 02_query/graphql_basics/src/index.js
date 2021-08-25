f9f4738267667ea6b7224b5f3a58ae5789a69139
SOunit <workbookwork7@gmail.com>
2021-08-25 06:53:01 -0700

    setup local graphql

--- 02_query/graphql_basics/src/index.js
+++ 02_query/graphql_basics/src/index.js
@@ -1,1 +1,36 @@
-console.log('hi there');
+import { GraphQLServer } from 'graphql-yoga';
+
+// Type Definition (Schema)
+const typeDefs = `
+    type Query {
+        hello: String!
+        name: String!
+        location: String!
+        bio: String!
+    }
+`;
+
+// Resolvers
+const resolvers = {
+  Query: {
+    hello() {
+      return 'hello';
+    },
+    name() {
+      return 'Jack Pearson';
+    },
+    location() {
+      return 'Canada';
+    },
+    bio() {
+      return 'This is us!';
+    },
+  },
+};
+
+const server = new GraphQLServer({ typeDefs, resolvers });
+
+// default port: 4000
+server.start(() => {
+  console.log('the server is up');
+});
