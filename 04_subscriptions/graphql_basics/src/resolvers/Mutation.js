a5d9f28d9a0d6419fbf8af57dbd22477968115d3
SOunit <workbookwork7@gmail.com>
2021-08-26 14:55:22 -0700

    add comment subscription

--- 04_subscriptions/graphql_basics/src/resolvers/Mutation.js
+++ 04_subscriptions/graphql_basics/src/resolvers/Mutation.js
@@ -123,7 +123,7 @@ const Mutation = {
     return post;
   },
 
-  createComment(parent, args, { db }, info) {
+  createComment(parent, args, { db, pubsub }, info) {
     const userExists = db.users.some((user) => user.id === args.data.author);
     const postExists = db.posts.some(
       (post) => post.id === args.data.post && post.published
@@ -137,6 +137,10 @@ const Mutation = {
       id: uuidv4(),
     };
     db.comments.push(comment);
+
+    pubsub.publish(`comment ${args.data.post}`, {
+      comment,
+    });
 
     return comment;
   },
